b92bf54b89b7fb8ac0375729c1a75b29401a8c81
SurfingOwl <arthur.cesar.fr@gmail.com>
2023-04-24 00:31:44 +0200

    cleaning

--- src/main/java/com/esgi/pa/api/dtos/requests/CreateLobbyRequest.java
+++ src/main/java/com/esgi/pa/api/dtos/requests/CreateLobbyRequest.java
@@ -5,16 +5,11 @@ import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.Builder;
-
-@Builder
 @JsonAutoDetect(fieldVisibility = ANY)
 public record CreateLobbyRequest(
     String name,
     UUID user,
     UUID game,
-    @JsonProperty("private")
     boolean isPrivate
 ) {}
