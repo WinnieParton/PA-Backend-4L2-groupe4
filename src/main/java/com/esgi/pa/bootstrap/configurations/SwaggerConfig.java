84ae29702c74deec0129029c7b68258f72d0fe5c
SurfingOwl <arthur.cesar.fr@gmail.com>
2023-06-20 10:46:57 +0200

    fix(security): swagger broken

--- src/main/java/com/esgi/pa/bootstrap/configurations/SwaggerConfig.java
+++ src/main/java/com/esgi/pa/bootstrap/configurations/SwaggerConfig.java
@@ -13,7 +13,6 @@ import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -28,8 +27,8 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()))
+            .securityContexts(List.of(securityContext()))
+            .securitySchemes(List.of(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
@@ -44,7 +43,7 @@ public class SwaggerConfig {
         AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+        return List.of(new SecurityReference("Bearer", authorizationScopes));
     }
 
 }
