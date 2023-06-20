6d5eb8b14597667228e4f70788243a7c8fe7b221
SurfingOwl <arthur.cesar.fr@gmail.com>
2023-06-20 14:38:32 +0200

    fix(error): config scan errorHandler

--- src/main/java/com/esgi/pa/bootstrap/configurations/ApiConfig.java
+++ src/main/java/com/esgi/pa/bootstrap/configurations/ApiConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com.esgi.pa.api.resources")
-public class ResourceConfig {
+@ComponentScan(basePackages = "com.esgi.pa.api")
+public class ApiConfig {
 }
--- src/main/java/com/esgi/pa/bootstrap/configurations/CorsConfig.java
+++ src/main/java/com/esgi/pa/bootstrap/configurations/CorsConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
