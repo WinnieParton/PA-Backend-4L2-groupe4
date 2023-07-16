4f59bfe87f28b312814cde076ac7e0ae893a95a0
Winnie Koum <wkoumwonkoagni@myges.fr>
2023-07-16 19:04:50 +0200

    test deploy continu

--- src/main/java/com/esgi/pa/api/resources/AuthResource.java
+++ src/main/java/com/esgi/pa/api/resources/AuthResource.java
@@ -58,8 +58,8 @@ public class AuthResource {
     );
   }
 
-  @GetMapping("/hello")
-  public String processGetPrivateMessage() {
-    return "hello monel ca va?";
-  }
+//   @GetMapping("/hello")
+//   public String processGetPrivateMessage() {
+//     return "hello monel ca va?";
+//   }
 }
