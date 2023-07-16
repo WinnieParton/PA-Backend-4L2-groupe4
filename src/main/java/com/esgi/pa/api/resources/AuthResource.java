364f073d35d80c22504f715adf570476b176c76d
Winnie Koum <wkoumwonkoagni@myges.fr>
2023-07-16 16:05:38 +0200

    jout d'api pour tester le deploiement continu

--- src/main/java/com/esgi/pa/api/resources/AuthResource.java
+++ src/main/java/com/esgi/pa/api/resources/AuthResource.java
@@ -1,5 +1,18 @@
 package com.esgi.pa.api.resources;
 
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+
+import javax.validation.Valid;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.esgi.pa.api.dtos.requests.auth.CreateUserRequest;
 import com.esgi.pa.api.dtos.requests.auth.UserLoginRequest;
 import com.esgi.pa.api.dtos.responses.auth.AuthenticationUserResponse;
@@ -7,15 +20,9 @@ import com.esgi.pa.api.mappers.UserMapper;
 import com.esgi.pa.domain.exceptions.TechnicalFoundException;
 import com.esgi.pa.domain.exceptions.TechnicalNotFoundException;
 import com.esgi.pa.domain.services.AuthService;
+
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.OK;
 
 @Validated
 @RestController
@@ -24,24 +31,35 @@ import static org.springframework.http.HttpStatus.OK;
 @Api(tags = "Authentication API")
 public class AuthResource {
 
-    private final AuthService authService;
+  private final AuthService authService;
 
-    @PostMapping(value = "login")
-    @ResponseStatus(OK)
-    public AuthenticationUserResponse login(@RequestBody @Valid UserLoginRequest request) throws TechnicalNotFoundException {
-        return UserMapper.toAuthenticationUserResponse(
-            authService.login(request.email(), request.password()));
-    }
+  @PostMapping(value = "login")
+  @ResponseStatus(OK)
+  public AuthenticationUserResponse login(
+    @RequestBody @Valid UserLoginRequest request
+  ) throws TechnicalNotFoundException {
+    return UserMapper.toAuthenticationUserResponse(
+      authService.login(request.email(), request.password())
+    );
+  }
 
-    @PostMapping(value = "signup")
-    @ResponseStatus(CREATED)
-    public AuthenticationUserResponse signup(@Valid @RequestBody CreateUserRequest request) throws TechnicalFoundException {
-        return UserMapper.toAuthenticationUserResponse(
-            authService.create(
-                request.name(),
-                request.email(),
-                request.password(),
-                request.role()));
-    }
+  @PostMapping(value = "signup")
+  @ResponseStatus(CREATED)
+  public AuthenticationUserResponse signup(
+    @Valid @RequestBody CreateUserRequest request
+  ) throws TechnicalFoundException {
+    return UserMapper.toAuthenticationUserResponse(
+      authService.create(
+        request.name(),
+        request.email(),
+        request.password(),
+        request.role()
+      )
+    );
+  }
 
+  @GetMapping("/hello")
+  public String processGetPrivateMessage() {
+    return "hello monel ca va?";
+  }
 }
